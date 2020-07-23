551ee07fc3f5477ae1b36f4712dfcb09424ad7fe
miles <dkmk1149@gmail.com>
2020-07-22 23:46:53 -0400

    Trying to create a flatten method with recursion. Failure tonight :(

--- recursion.rb
+++ recursion.rb
@@ -14,3 +14,43 @@ def palindrome(str)
   end
 end
 
+# Bottlees of beer on the wall?
+
+def bottles(n)
+  if n == 0 
+   puts 'no more bottles of beer on the wall'
+  else
+    puts "#{n} bottles of beer on the wall "
+    bottles(n-1)
+  end
+end
+
+def fib(n)
+  if n == 0
+    0
+  elsif n == 1
+    1
+  else
+    fib(n-2) + fib(n-1)
+  end
+end
+
+# flattens an array
+def flatten(arr)
+  if arr[0].empty?
+    arr.delete_at(0)
+  end
+  if arr.none? {|a| a.is_a?(Array)}
+    arr
+  else
+   
+    arr.push(arr[0].delete_at(0))
+    flatten(arr)
+    
+  end
+end
+
+# flatten([[1,2],[3,4]]) => [1,2,3,4]
+# flatten([[1,[8,9]],[3,4]]) => ERROR
+
+
