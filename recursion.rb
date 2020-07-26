d32ccfdd645edaeef88b9e9bd297fc29e9e7a9fa
miles <dkmk1149@gmail.com>
2020-07-26 18:40:53 -0400

    Roman numerals w/ recursion and Fibonacci w/ recursion

--- recursion.rb
+++ recursion.rb
@@ -47,9 +47,69 @@ def flatten(arr, flat = [])
   flat
 end
 
-p flatten([[1,2],[3,4]])
-
 # flatten([[1,2],[3,4]]) => [1,2,3,4]
 # flatten([[1,[8,9]],[3,4]]) => [1,8,9,3,4]
+
+# Converts a number to a roman numeral
+def roman(num, comp = '')
+  roman_mapping = {
+  1000 => "M",
+  900 => "CM",
+  500 => "D",
+  400 => "CD",
+  100 => "C",
+  90 => "XC",
+  50 => "L",
+  40 => "XL",
+  10 => "X",
+  9 => "IX",
+  5 => "V",
+  4 => "IV",
+  1 => "I"
+}
+ 
+  if num == 0
+    p comp 
+  else
+    roman_mapping.each_key do |key|
+      result = num - key 
+      if result >= 0 
+        comp += roman_mapping[key]
+        roman(result, comp)
+        break
+      end
+    end
+  end
+end
+
+# fibonacci sequence without recursion 
+def fibs(num)
+  fib = []
+  0.upto(num-1) do |i|
+    if i == 0
+      fib.push(0)
+    elsif i == 1
+      fib.push(1)
+    else
+      fib.push(fib[i-2]+fib[i-1])
+    end
+  end
+  fib_to_s = fib.join(", ")
+  fib_to_s
+end
+
+# fibonacci sequence with recursion
+def fibs_rec(num, fib = [0,1])
+  if num == 1
+    0
+  elsif num == fib.length
+    fib
+  else
+    result = fib[fib.length - 2] + fib[fib.length - 1]
+    fib.push(result)
+    fibs_rec(num, fib)
+  end
+end
+
 
 
